c51d81bdc8ca361ed2f2f52d0d3b826278cbc95d
OGURA DAIKI <dkog2001@gmail.com>
2022-10-19 23:23:54 +0900

    serviceworkerをすぐ有効化するように

--- sw.js
+++ sw.js
@@ -1,15 +1,10 @@
-function sleep(ms) {
-  const startTime = performance.now();
-  while (performance.now() - startTime < ms);
-}
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    new Promise((resolve) => {
-      sleep(5000);
-      resolve();
-    })
-  );
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", e=>{
