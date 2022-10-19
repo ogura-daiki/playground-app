2c6f10d571145490104171939e4215f925617b0c
OGURA DAIKI <dkog2001@gmail.com>
2022-10-19 23:12:27 +0900

    activateイベント追加

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
+function sleep(ms) {
+  const startTime = performance.now();
+  while (performance.now() - startTime < ms);
+}
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    new Promise((resolve) => {
+      sleep(5000);
+      resolve();
+    })
+  );
+});
+
 self.addEventListener("fetch", e=>{
   console.log(e.request.url);
   e.respondWith(fetch(e.request));
