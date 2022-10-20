bb5ec5f94b571420cb7bdd04be744f8cf7b705e4
OGURA DAIKI <dkog2001@gmail.com>
2022-10-20 21:17:57 +0900

    仮想ファイル返送部分を変更

--- sw.js
+++ sw.js
@@ -9,16 +9,38 @@ self.addEventListener('activate', (event) => {
 
 importScripts("./isLocalhost.js");
 
-self.addEventListener("fetch", e=>{
-  console.log(isLocalhost);
-  if(new URL(e.request.url).pathname.split("/").map(v=>v.trim()).filter(v=>v).pop() === "test2.html"){
-    e.respondWith(new Response(new Blob([`Upgrade or make this repository public to enable Pages
+const responseFromData = ({value, type}) => new Response(new Blob([value], {type}), {status:200, statusText:"OK"});
+const files = new Map(Object.entries({
+  "/test2.html":{
+    type:"text/html",
+    value:`
+    Upgrade or make this repository public to enable Pages
     GitHub Pages is designed to host your personal, organization, or project pages from a GitHub repository.
-    
+      
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jschardet/3.0.0/jschardet.min.js"></script>
     <script>
         console.log(jschardet);
-    </script>`], {type:"text/html"}), {status:200, statusText:"OK", }));
+    </script>
+    `,
+  },
+}));
+const getFile = (url)=>{
+  const baseURL = location.origin+(isLocalhost?"":"/playground-app");
+  if(!url.startsWith(baseURL)) return;
+  const path = url.slice(baseURL.length);
+  if(!files.has(path)) return;
+  const data = files.get(path);
+  if(typeof data === "function"){
+    return data();
+  }
+  return responseFromData(data);
+}
+
+self.addEventListener("fetch", e=>{
+  console.log(e.request.url);
+  const fileResponse = getFile(e.request.url);
+  if(fileResponse){
+    e.respondWith(fileResponse);
     return;
   }
   e.respondWith(fetch(e.request));
