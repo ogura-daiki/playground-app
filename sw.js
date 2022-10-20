52b9ea37a5c6ac0c7b13affd69c3094edbfd52ea
OGURA DAIKI <dkog2001@gmail.com>
2022-10-20 19:28:38 +0900

    fetchの割り込みで生成したページを読み込むiframeの通信を傍受できるかテスト

--- sw.js
+++ sw.js
@@ -9,5 +9,15 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener("fetch", e=>{
   console.log(e.request.url);
+  if(new URL(e.request.url).pathname.split("/").map(v=>v.trim()).filter(v=>v).pop() === "test2.html"){
+    e.respondWith(new Response(new Blob([`Upgrade or make this repository public to enable Pages
+    GitHub Pages is designed to host your personal, organization, or project pages from a GitHub repository.
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jschardet/3.0.0/jschardet.min.js"></script>
+    <script>
+        console.log(jschardet);
+    </script>`], {type:"text/html"}), {status:200, statusText:"OK", }));
+    return;
+  }
   e.respondWith(fetch(e.request));
 })
