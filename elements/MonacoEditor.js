156f094e576aa68e754bbe14f28f8ce1f30ea570
OGURA DAIKI <dkog2001@gmail.com>
2022-10-25 23:19:31 +0900

    MonacoEditorを分離

---
+++ elements/MonacoEditor.js
@@ -0,0 +1,80 @@
+await new Promise(resolve=>{
+  const script = document.createElement("script");
+  script.addEventListener("load", e=>resolve());
+  script.src = "https://unpkg.com/monaco-editor@latest/min/vs/loader.js";
+  document.body.append(script);
+});
+
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
+const proxy = URL.createObjectURL(new Blob([`
+  self.MonacoEnvironment = {
+    baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+  };
+  importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+`], { type: 'text/javascript' }));
+window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+await new Promise(resolve=>{require(["vs/editor/editor.main"], resolve)});
+
+class MonacoEditor extends HTMLElement{
+  #file={};
+  #editor;
+  constructor(){
+    super();
+    this.theme="vs-dark";
+    this.attachShadow({mode:"open"});
+    this.shadowRoot.innerHTML=`
+    <style>${this.constructor.styles}</style>
+    ${this.render()}
+    `;
+  }
+  static get styles(){
+    return `
+      :host{
+        display:block;
+      }
+      #editor{
+        display:block;
+        position:absolute;
+        top:0px;
+        left:0px;
+        margin:0px;
+        padding:0px;
+        width:100%;
+        height:100%;
+        overflow:hidden;
+      }
+    `;
+  }
+  render(){
+    return `
+    <link rel="stylesheet" href="https://unpkg.com/monaco-editor@0.34.0/min/vs/editor/editor.main.css"></link>
+    <div id=editor></div>
+    `
+  }
+  update() {
+    if(this.#editor){
+      this.#editor.getModel().setValue(this.#file.stringValue);
+      monaco.editor.setModelLanguage(this.#editor.getModel(), getLanguageFromFileName(this.#file.name));
+      monaco.editor.setTheme(this.theme);
+      return;
+    }
+    this.#editor = monaco.editor.create(this.shadowRoot.querySelector("#editor"), {
+      value: this.#file.stringValue,
+      language: getLanguageFromFileName(this.#file.name),
+      theme: this.theme,
+      automaticLayout: true,
+    });
+    this.#editor.getModel().onDidChangeContent((event) => {
+      this.dispatchEvent(new CustomEvent("updateValue", {detail:{file:this.#file, newVal:this.#editor.getValue()}}));
+    });
+  }
+  set file(val){
+    console.log("updated")
+    this.#file = val;
+    this.update();
+  }
+  get file(){
+    return this.#file;
+  }
+}
+customElements.define("monaco-editor", MonacoEditor);
--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 import { LitElement, html, css } from 'https://unpkg.com/lit-element/lit-element.js?module';
 import make from "./libs/make.js";
 import {Store, createFile, createFolder, newProject} from "./libs/Store.js";
+import "./elements/MonacoEditor.js";
+
 
 const iconFonts = css`
 /**
@@ -1144,86 +1146,3 @@ class Split extends LitElement {
 }
 
 customElements.define("split-panel", Split);
-
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
-const proxy = URL.createObjectURL(new Blob([`
-  self.MonacoEnvironment = {
-    baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
-  };
-  importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
-`], { type: 'text/javascript' }));
-window.MonacoEnvironment = { getWorkerUrl: () => proxy };
-const loadEditor = new Promise(resolve=>{require(["vs/editor/editor.main"], resolve)});
-
-class MonacoEditor extends HTMLElement{
-  static get properties(){
-    return {
-      filename:{type:String},
-      theme:{type:String},
-      _update:{state:true},
-    };
-  }
-  #file;
-  #editor;
-  constructor(){
-    super();
-    this.#file={};
-    this.theme="vs-dark";
-    this.attachShadow({mode:"open"});
-    this.shadowRoot.innerHTML=`
-    <style>${this.constructor.styles}</style>
-    ${this.render()}
-    `;
-  }
-  static get styles(){
-    return css`
-      :host{
-        display:block;
-      }
-      #editor{
-        display:block;
-        position:absolute;
-        top:0px;
-        left:0px;
-        margin:0px;
-        padding:0px;
-        width:100%;
-        height:100%;
-        overflow:hidden;
-      }
-    `;
-  }
-  render(){
-    return `
-    <link rel="stylesheet" href="https://unpkg.com/monaco-editor@0.34.0/min/vs/editor/editor.main.css"></link>
-    <div id=editor></div>
-    `
-  }
-  update() {
-    loadEditor.then(e=>{
-      if(this.#editor){
-        this.#editor.getModel().setValue(this.#file.stringValue);
-        monaco.editor.setModelLanguage(this.#editor.getModel(), getLanguageFromFileName(this.#file.name));
-        monaco.editor.setTheme(this.theme);
-        return;
-      }
-      this.#editor = monaco.editor.create(this.shadowRoot.querySelector("#editor"), {
-        value: this.#file.stringValue,
-        language: getLanguageFromFileName(this.#file.name),
-        theme: this.theme,
-        automaticLayout: true,
-      });
-      this.#editor.getModel().onDidChangeContent((event) => {
-        this.dispatchEvent(new CustomEvent("updateValue", {detail:{file:this.#file, newVal:this.#editor.getValue()}}));
-      });
-    });
-  }
-  set file(val){
-    this.#file = val;
-    this.update();
-  }
-  get file(){
-    return this.#file;
-  }
-}
-customElements.define("monaco-editor", MonacoEditor);
