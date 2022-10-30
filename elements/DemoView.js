a8ff47e17a60be98f0720a30ccfafdf95254756d
OGURA DAIKI <dkog2001@gmail.com>
2022-10-30 17:03:40 +0900

    DemoViewをLitElementを使用して書き換え、generateDemoPageに時間がかかった場合のためにローディング表示を追加

--- elements/DemoView.js
+++ elements/DemoView.js
@@ -1,24 +1,21 @@
 import generateDemoPage from "../libs/generateDemoPage.js";
+import { LitElement, html, css, when, until } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-class DemoView extends HTMLElement {
-  constructor() {
+class DemoView extends LitElement {
+  static get properties(){
+    return {
+      _project:{type:Object},
+    };
+  }
+  constructor(){
     super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-    <style>${this.constructor.style}</style>
-    <iframe id="iframe" sandbox="allow-scripts allow-modals allow-downloads allow-popups">
-    `;
+    this._project = undefined;
   }
-  set project(project) {
-    generateDemoPage(project).then(e=>this.shadowRoot.querySelector("#iframe").srcdoc = e);
-  }
-  sendMessage(msg) {
-    this.shadowRoot.querySelector("#iframe").contentWindow.postMessage(msg, "*");
-  }
-  static get style() {
-    return `
+  static get styles() {
+    return css`
       :host{
         display:block;
+        background:white;
       }
       html,body{
         width:100%;
@@ -33,6 +30,62 @@ class DemoView extends HTMLElement {
       }
     `;
   }
+  #renderDemo(){
+    const loading = `
+    <style>
+      html,body{
+        width:100%;
+        height:100%;
+        margin:0px;
+      }
+      #loading{
+        width:100%;
+        height:100%;
+        display:grid;
+        place-items:center;
+      }
+      #loading .circle{
+        width:4rem;height:4rem;border:.5rem solid rgb(12, 138, 255);border-radius:50%;
+        border-right-color: rgb(12, 138, 255, .2);
+        animation:spin infinite both 1s linear, fade_in 1 both 1s .5s;
+      }
+      @keyframes spin {
+        from{transform:rotateZ(0deg)}
+        to{transform:rotateZ(360deg)}
+      }
+      @keyframes fade_in {
+        from{opacity:0}
+        to{opacity:1}
+      }
+    </style>
+    <div id="loading">
+      <div class="circle"></div>
+    </div>
+    `;
+    return until(generateDemoPage(this._project), loading);
+  }
+  render(){
+    console.log(this._project);
+    return html`
+    <iframe
+      id="iframe"
+      sandbox="allow-scripts allow-modals allow-downloads allow-popups"
+      .srcdoc=${when(this._project, ()=>this.#renderDemo(), ()=>"")}
+    >
+    `;
+  }
+  set project(project) {
+    console.log({project})
+    const before = this._project;
+    this._project = project;
+    if(before === project){
+      this.requestUpdate();
+    }
+  }
+  sendMessage(msg) {
+    this.renderRoot.querySelector("#iframe").contentWindow.postMessage(msg, "*");
+  }
 }
+
 customElements.define("demo-view", DemoView);
 
